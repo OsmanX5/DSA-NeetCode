b8f1f9f2a8abbd53faccf28337dd32abbeeec756
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2026-04-08 21:31:11 +0300

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int max =0;
+        int n = prices.Length;
+        int holded = prices[0];
+        foreach(int x in prices){
+            max = Math.Max(x-holded,max);
+            if(x>holded);
+                
+            else
+                holded = x;
+        }
+        return max;
+    }
+}
